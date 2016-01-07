3a191ba59413f5e226c830799dbda981c0914d83
Jayson Stemmler <jdstemmler@gmail.com>
2016-01-07 13:15:00 -0800

    update outside to include the forecast

--- loggers/outside_temp_logger.py
+++ loggers/outside_temp_logger.py
@@ -6,12 +6,17 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from analytics import record_current_outside_temperature
+from analytics import record_forecast_data
 from pymongo import MongoClient
 
 if __name__ == "__main__":
 
     client = MongoClient()
     db = client['homewatch']
-    table = db['outside_temp']
+    otable = db['outside_temp']
+    ftable = db['forecast_data']
 
-    record_current_outside_temperature(table)
+    ftable.drop()
+
+    record_current_outside_temperature(otable)
+    record_forecast_data(ftable)
